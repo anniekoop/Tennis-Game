505ea75eb2c3a18ec23e5d765f8d346175b7caf3
Annie Koop <164425639+anniekoop@users.noreply.github.com>
2024-06-07 18:30:38 -0600

    Update game.js

--- game.js
+++ game.js
@@ -16,8 +16,8 @@ let playerPaddleY = (canvasHeight - paddleHeight) / 2;
 let computerPaddleY = (canvasHeight - paddleHeight) / 2;
 let ballX = canvasWidth / 2;
 let ballY = canvasHeight / 2;
-let ballSpeedX = 7;
-let ballSpeedY = 7;
+let ballSpeedX = 8.5;
+let ballSpeedY = 8.5;
 
 let playerScore = 0;
 let computerScore = 0;
